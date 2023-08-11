72f30ef1ec2af9955fa3ced84c2e78564d9e21ff
H-Asmaa <asmaehadar32@gmail.com>
2023-08-11 15:02:32 +0100

    Committing the new class FileStorage with the save and reload, new and all methods and modification of base_model.py

---
+++ models/engine/file_storage.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+0x00. AirBnB clone - The console
+"""
+import json
+import os
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """
+    Class documentation -- TO BE ADDED --
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """The method all that returns the objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """The method new that sets the key of the dictionary of the objects"""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """The method save that saves the new objects as str in a file"""
+        with open(self.__file_path, "w") as file:
+            serialized = {}
+            for key, val in self.__objects.items():
+                serialized = val.to_dict()
+            file.write(json.dumps(serialized))
+
+    def reload(self):
+        """
+        The method reload that reloads the dictionary from file and
+        creates obj from it
+        """
+        """REMEMBER TO WRITE IT AGAIN"""
+        if os.path.isfile(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+                obj_dict = json.load(f)
+                self.__objects = {
+                    k: self.classes()[v["__class__"]](**v)
+                    for k, v in obj_dict.items()
+                }
