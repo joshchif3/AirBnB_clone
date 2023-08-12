cca52f4edbeecb0a3a143e9e3e1a92136b9af4fe
H-Asmaa <asmaehadar32@gmail.com>
2023-08-12 18:46:54 +0100

    Committing a fix for the to_dict function that was not updated after updating the __init__ method

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@
 """
 import uuid
 from datetime import datetime
-from models import storage
+# from models import storage
 
 
 class BaseModel:
@@ -70,7 +70,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            # storage.new(self)
 
     def __str__(self):
         """A string representation of an object using the method __str__"""
@@ -79,15 +79,12 @@ class BaseModel:
     def save(self):
         """An update to the attribute updated_at using the method save"""
         self.updated_at = datetime.now()
-        storage.save()
+        # storage.save()
 
     def to_dict(self):
         """A dictionary representation of an object using the method to_dict"""
-        dictionnary = {"my_number": self.my_number,
-                       "name": self.name,
-                       "updated_at": self.updated_at.isoformat(),
-                       "__class__": self.__class__.__name__,
-                       "id": self.id,
-                       "created_at": self.created_at.isoformat()
-                       }
+        dictionnary = self.__dict__.copy()
+        dictionnary["__class__"] = self.__class__.__name__
+        dictionnary["created_at"] = self.updated_at.isoformat()
+        dictionnary["updated_at"] = self.updated_at.isoformat()
         return dictionnary
