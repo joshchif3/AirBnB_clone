1d71f7414d9e02bf39be2d950f2fd50856d5fc66
H-Asmaa <asmaehadar32@gmail.com>
2023-08-14 03:23:45 +0100

    Committing tests verification

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -19,24 +19,29 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(BaseModel, type(instance))
 
     def test_instance_id(self):
+        "testing"
         instance = BaseModel()
         self.assertEqual(str, type(instance.id))
 
     def test_instance_id(self):
+        "testing"
         instance = BaseModel()
         self.assertEqual(datetime, type(instance.created_at))
 
     def test_instance_id_unique(self):
+        "testing"
         instance_1 = BaseModel()
         instance_2 = BaseModel()
         self.assertNotEqual(instance_1.id, instance_2.id)
 
     def test_instance_str(self):
+        "testing"
         instance = BaseModel()
         expected_str = f"[BaseModel] ({instance.id}) {instance.__dict__}"
         self.assertEqual(expected_str, instance.__str__())
 
     def test_instance_created_at(self):
+        "testing"
         instance = BaseModel()
         self.assertEqual(datetime, type(instance.created_at))
 
@@ -47,13 +52,19 @@ class TestBaseModel(unittest.TestCase):
         instance.my_number = 89
         self.assertEqual(instance.name, "My First Model")
         self.assertEqual(instance.my_number, 89)
-"""
-# The save method isn't working.
-    def test_instance_save(self):
-        instance = BaseModel()
-        initial_value = instance.updated_at
-        instance.save()
-        updated_value = instance.updated_at
-        self.assertNotEqual(initial_value, updated_value)
-"""
 
+    def test_object_to_dict(self):
+        """Testing To_dict method"""
+        instance = BaseModel()
+        instance.name = "My First Model"
+        instance.my_number = 89
+        dictionary = instance.to_dict()
+        dictionary_cmp = {
+            'my_number': 89,
+            'name': 'My First Model',
+            '__class__': 'BaseModel',
+            'updated_at': instance.updated_at.isoformat(),
+            'id': instance.id,
+            'created_at': instance.created_at.isoformat()
+        }
+        self.assertEqual(dictionary_cmp, dictionary)
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 """
 0x00. AirBnB clone - The console
+TEST CASES
 """
-from models.base_model import BaseModel
+
+import unittest
+from models.user import User
+from datetime import datetime
+import pep8
 
 
-class User(BaseModel):
+class TestBaseModel(unittest.TestCase):
     """
-    Class User that inherits from BaseModel
-        - Update FileStorage to manage correctly serialization and
-        deserialization of User.
-        - Update your command interpreter (console.py) to allow show,
-        create, destroy, update and all used with User.
+    TESTING CLASS
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
 
-    def __init__(self, *args, **kwargs):
-        """The init method for the User class"""
-        super().__init__(*args, **kwargs)
+    def test_pep8_compliance(self):
+        """ Test PEP8 compliance using pycodestyle"""
+        pycodestyle = pep8.StyleGuide(quiet=True)
+        file_paths = ["models/user.py"]  # Add more file paths if needed
+        result = pycodestyle.check_files(file_paths)
+        error_message = "Found code style errors (and warnings)."
+        self.assertEqual(result.total_errors, 0, error_message)
