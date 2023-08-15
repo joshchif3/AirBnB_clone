b0517b5bff10408bab7b4d568c32fedd0ea9a6af
H-Asmaa <asmaehadar32@gmail.com>
2023-08-15 01:19:20 +0100

    Committing a test for file storage

---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+0x00. AirBnB clone - The console
+TEST CASES
+"""
+import unittest
+from models.engine.file_storage import FileStorage
+from datetime import datetime
+
+
+class TestFileStorage(unittest.TestCase):
+    """
+    TESTING CLASS
+    """
+    pass
